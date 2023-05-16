da071c4ffca735ef7270c802ac1fc5760682efc0
Justin Dorsa <justin.dorsa@paramount.com>
2023-05-16 14:14:54 -0400

    Test 1

--- Test_1/Test_1.cs
+++ Test_1/Test_1.cs
@@ -68,7 +68,7 @@ namespace Test_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-		engine.GenerateInformation("Whats up")
+		engine.GenerateInformation("Yo")
 		}
 	}
 }

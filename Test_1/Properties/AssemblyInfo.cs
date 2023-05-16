0d27deac13f25bc5d9c9ad883265297aa95bf5ca
Lappy <Lappy@DESKTOP-12U1MQH>
2023-05-16 14:09:10 -0400

    Add project files.

---
+++ Test_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Test_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F7FF8987-853E-4B64-A716-C353AFE17058")]
+[assembly: AssemblyVersion("1.0.0.0")]
